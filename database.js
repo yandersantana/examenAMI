9838e2ff02db69f694ae0970e94da9da1c1ceb20
dany94xd <danimaxi60@hotmail.com>
2019-06-18 12:11:25 -0500

    cambio ruta

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/clientes';
+//const URI = 'mongodb://localhost/clientes';
+
+const URI = 'mongodb://root:ce42286ddaba98f3@162.212.130.145:7822/clientes';
+
 
 mongoose.connect(URI)
     .then(db => console.log('db is connected'))
