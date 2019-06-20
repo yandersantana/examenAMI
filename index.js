f8ec2c84dcac3d172ea9da25b7d6c3361fd095b1
dany94xd <danimaxi60@hotmail.com>
2019-06-20 09:25:51 -0500

    index.js lineas para produccion

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 //app.use('/',express.static('client',{redirect:false}));
-//app.use(cors({origin: 'http://162.212.130.145'}));
-app.use(cors({origin: 'http://loaclhost:4200'}));
+app.use(cors({origin: 'http://162.212.130.145'}));
+//app.use(cors({origin: 'http://loaclhost:4200'}));
 
 app.use(function (req, res, next) {
 
@@ -27,9 +27,9 @@ app.use('/api/employees', require('./routes/employee.routes'));
 
 // starting the server
 
-// app.get('*',function(req,res,next){
-//     res.sendFile(path.resolve('client/index.html'))
-//   });
+ app.get('*',function(req,res,next){
+     res.sendFile(path.resolve('client/index.html'))
+   });
   
 app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
