1b46027d0c44ce6cb737bdb0a3c166d32b1bd1c1
dany94xd <danimaxi60@hotmail.com>
2019-06-27 12:41:51 -0500

    uff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var path = require('path')
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
-app.use('/',express.static('client',{redirect:false}));
+//app.use('/',express.static('client',{redirect:false}));
 //app.use(cors({origin: 'http://162.212.130.145:3000/api/employees'}));
 app.use(cors({origin: 'http://localhost:4200'}));
 
@@ -30,9 +30,9 @@ app.use('/api/employees', require('./routes/employee.routes'));
 
 //starting the server
 
-  app.get('*',function(req,res,next){
-      res.sendFile(path.resolve('client/index.html'))
-    });
+  // app.get('*',function(req,res,next){
+  //     res.sendFile(path.resolve('client/index.html'))
+  //   });
   
 app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
