fa3492e4f0eb132a74759edf698bdefadd769805
dany94xd <danimaxi60@hotmail.com>
2019-07-07 22:02:23 -0500

    cors arreglado

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 app.use('/',express.static('client',{redirect:false}));
-app.use(cors({origin: 'http://162.212.130.145:3000/api/employees'}));
+app.use(cors({origin: 'http://162.212.130.145'}));
 //app.use(cors({origin: 'http://localhost:4200'}));
 
 app.use(function (req, res, next) {
