e405e96ca5972db684043b747fd36d18da57a9a3
dany94xd <danimaxi60@hotmail.com>
2019-07-24 12:23:27 -0500

    update app

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -1,22 +1,27 @@
-const Persona = require('../models/persona');
 const Usuario = require('../models/usuario');
+const Persona = require('../models/persona');
 
 const mobileAppCtrl = {};
 
 mobileAppCtrl.getUsuarioData = async (req, res, next) => {
-    const { user } = req.params;
+    const { id } = req.params;
     //const usuario = await Usuario.findById(id);
-    const usuarioData = await Usuario.findOne({where: {name:'user'}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
-    res.json(usuarioData);
+    //{ 'username': userName }
+    //const usuarioData = await Usuario.findOne({where: {user:'user'}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
+    const usuarioData = await Usuario.findOne({ 'user': id });
+    //res.json(usuarioData);
+    res.json(usuarioData.user, usuarioData.nroBotellas, usuarioData.saldoTotal, usuarioData.saldoActual);
+
 };
 //la puta madre
 mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     const { id } = req.params;
-    const usuarioData = await Usuario.findOne({where: {idInstitucion:matricula}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
-
- const personaData = await Persona.findOne({where: {idInstitucion:matricula}, select: ['nombre', 'apellido']});
-
-    res.json({nombre:personaData.nombre, apellido:personaData.apellido, nroBotellas:usuarioData.nroBotellas, saldoTotal:usuarioData.saldoTotal});
+    //const usuarioData = await Usuario.findOne({where: {idInstitucion:matricula}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
+    const usuarioData = await Usuario.findOne({ 'idInstitucion': id });
+ //const personaData = await Persona.findOne({where: {idInstitucion:matricula}, select: ['nombre', 'apellido']});
+ const personaData = await Usuario.findOne({ 'idInstitucion': id });
+    //res.json({nombre:personaData.nombre, apellido:personaData.apellido, nroBotellas:usuarioData.nroBotellas, saldoTotal:usuarioData.saldoTotal});
+    res.json(personaData.nombre, personaData.apellido, usuarioData.nroBotellas, usuarioData.saldoTotal);
 };
 
 //mobileAppCtrl.getSaldoResponse = async (req, res, next) => {
