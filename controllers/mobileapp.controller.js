c45ae68e743e9c1bb78e55cf835d5d1d7c179ac6
dany94xd <danimaxi60@hotmail.com>
2019-07-24 12:33:12 -0500

    persona app update

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -19,7 +19,7 @@ mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     //const usuarioData = await Usuario.findOne({where: {idInstitucion:matricula}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
     const usuarioData = await Usuario.findOne({ 'idInstitucion': id });
  //const personaData = await Persona.findOne({where: {idInstitucion:matricula}, select: ['nombre', 'apellido']});
- const personaData = await Usuario.findOne({ 'idInstitucion': id });
+ const personaData = await Persona.findOne({ 'idInstitucion': id });
     //res.json({nombre:personaData.nombre, apellido:personaData.apellido, nroBotellas:usuarioData.nroBotellas, saldoTotal:usuarioData.saldoTotal});
     res.json(personaData.nombre, personaData.apellido, usuarioData.nroBotellas, usuarioData.saldoTotal);
 };
