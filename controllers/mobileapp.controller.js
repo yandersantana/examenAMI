dd753d41ae51fc2d95f7cadc61150ce3d1bdf15d
dany94xd <danimaxi60@hotmail.com>
2019-07-24 15:26:48 -0500

    prueba

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -29,7 +29,7 @@ mobileAppCtrl.getUsuarioData = async (req, res, next) => {
 mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     const { matricula } = req.params;
     //const usuarioData = await Usuario.findOne({where: {idInstitucion:matricula}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
-    const usuarioData = await Usuario.find({ 'idInstitucion': matricula }).select({ idInstitucion: 1, NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
+    const usuarioData = await Usuario.find({ 'user': matricula }).select({ idInstitucion: 1, NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
  //const personaData = await Persona.findOne({where: {idInstitucion:matricula}, select: ['nombre', 'apellido']});
  //const personaData = await Persona.find({ 'idInstitucion': matricula }).select({ nombre: 1, apellido: 1});
     //res.json({nombre:personaData.nombre, apellido:personaData.apellido, nroBotellas:usuarioData.nroBotellas, saldoTotal:usuarioData.saldoTotal});
