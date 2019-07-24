1654452adea6ff9db702fddf2a8992de9eeadb4c
dany94xd <danimaxi60@hotmail.com>
2019-07-24 11:00:22 -0500

    ruta y controlado movilapp

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.use('/api/roles', require('./routes/rol.routes'));
 app.use('/api/eventos', require('./routes/evento.routes'));
 app.use('/api/instituciones', require('./routes/institucion.routes'));
 app.use('/api/opcionesmenu', require('./routes/opcionesmenu.routes'));
+app.use('/api/movilapp', require('./routes/mobileapp.routes'));
 
 
 
