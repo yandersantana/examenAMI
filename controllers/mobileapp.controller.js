5029660cae272785c784f8c37390a171153da94a
dany94xd <danimaxi60@hotmail.com>
2019-07-24 14:03:02 -0500

    kdfsad

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -9,7 +9,7 @@ mobileAppCtrl.getUsuarioData = async (req, res, next) => {
     //const usuario = await Usuario.findById(id);
     //{ 'username': userName }
     //const usuarioData = await Usuario.find({where: {'user':id}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
-    const usuarioData = await Usuario.find({ 'user': id }, select: ['nroBotellas', 'saldoTotal','saldoActual']);
+    const usuarioData = await Usuario.find({ where: {'user': id }, select: ['nroBotellas', 'saldoTotal','saldoActual']});
     const dataUsuario={
         usuario:usuarioData.user,
         botellas:usuarioData.nroBotellas,
