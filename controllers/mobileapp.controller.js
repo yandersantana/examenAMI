ff83fbe6ce2f176aac8a42979330d06ab9679859
dany94xd <danimaxi60@hotmail.com>
2019-07-24 15:49:21 -0500

    pilass

--- controllers/mobileapp.controller.js
+++ controllers/mobileapp.controller.js
@@ -29,12 +29,12 @@ mobileAppCtrl.getUsuarioData = async (req, res, next) => {
 mobileAppCtrl.getMatriculaData = async (req, res, next) => {
     const { id } = req.params;
     //const usuarioData = await Usuario.findOne({where: {idInstitucion:matricula}, select: ['nroBotellas', 'saldoTotal','saldoActual']});
-    const usuarioData = await Usuario.find({ 'idInstitucion': id}).select({ NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
+    const usuarioData = await Usuario.find({ 'idInstitucion': id}).select({ idInstitucion:1, NroBotellas: 1, saldoTotal: 1, saldoActual: 1 });
  //const personaData = await Persona.findOne({where: {idInstitucion:matricula}, select: ['nombre', 'apellido']});
- //const personaData = await Persona.find({ 'idInstitucion': matricula }).select({ nombre: 1, apellido: 1});
+ const personaData = await Persona.find({ 'idInstitucion': id }).select({ nombre: 1, apellido: 1});
     //res.json({nombre:personaData.nombre, apellido:personaData.apellido, nroBotellas:usuarioData.nroBotellas, saldoTotal:usuarioData.saldoTotal});
     console.log(usuarioData);
-    res.json(usuarioData);
+    res.json(usuarioData, personaData);
 };
 
 //mobileAppCtrl.getSaldoResponse = async (req, res, next) => {
