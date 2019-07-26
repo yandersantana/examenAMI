a23d2a11a400f4f8293d86d10266c15a1675395d
dany94xd <danimaxi60@hotmail.com>
2019-07-25 22:48:41 -0500

    require models

--- auth/auth.model.js
+++ auth/auth.model.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 const Schema = mongoose.Schema;
-const User= require('../../models/usuario')
+const User= require('../models/auth.model')
 mongoose.set('useCreateIndex',true) //solucionar advertencia por consola
 const userSchema = new Schema({
     user:{
