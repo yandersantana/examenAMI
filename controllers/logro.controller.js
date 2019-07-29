e318b405e5d40d40874452c71ec36d5cd35337a2
dany94xd <danimaxi60@hotmail.com>
2019-07-29 00:11:53 -0500

    lalalla2

--- controllers/logro.controller.js
+++ controllers/logro.controller.js
@@ -9,13 +9,15 @@ logroCtrl.getlogros = async (req, res, next) => {
 
 logroCtrl.createlogro = async (req, res, next) => {
     const logro = new logro({
-        name: req.body.name,
-        position: req.body.position,
-        office: req.body.office,
-        salary: req.body.salary
+        idLogro: req.body.idLogro,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        minbotella: req.body.minbotella,
+        maxbotella: req.body.maxbotella,
+        idInstitucion: req.body.idInstitucion
     });
     await logro.save();
-    res.json({status: 'logro created'});
+    res.json({ status: 'logro created' });
 };
 
 logroCtrl.getlogro = async (req, res, next) => {
@@ -27,18 +29,20 @@ logroCtrl.getlogro = async (req, res, next) => {
 logroCtrl.editlogro = async (req, res, next) => {
     const { id } = req.params;
     const logro = {
-        name: req.body.name,
-        position: req.body.position,
-        office: req.body.office,
-        salary: req.body.salary
+        idLogro: req.body.idLogro,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        minbotella: req.body.minbotella,
+        maxbotella: req.body.maxbotella,
+        idInstitucion: req.body.idInstitucion
     };
-    await logro.findByIdAndUpdate(id, {$set: logro}, {new: true});
-    res.json({status: 'logro Updated'});
+    await logro.findByIdAndUpdate(id, { $set: logro }, { new: true });
+    res.json({ status: 'logro Updated' });
 };
 
 logroCtrl.deletelogro = async (req, res, next) => {
     await logro.findByIdAndRemove(req.params.id);
-    res.json({status: 'logro Deleted'});
+    res.json({ status: 'logro Deleted' });
 };
 
 module.exports = logroCtrl;
